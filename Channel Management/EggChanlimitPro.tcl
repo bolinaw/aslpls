a3b542e16683415ccd54897c1e877f35663c9f6b
aslpls <kasikas.ako@gmail.com>
2026-06-23 15:52:47 +0400

    Rename chanlimit_pro.tcl to EggChanlimitPro.tcl

--- Channel Management/EggChanlimitPro.tcl
+++ Channel Management/EggChanlimitPro.tcl
@@ -8,7 +8,7 @@
 ##                                                                               06/2026 ##
 ###########################################################################################
 # 
-# chanlimit_pro.tcl - Advanced Dynamic Channel Limit Script                                 
+# EggChanlimitPro.tcl - Advanced Dynamic Channel Limit Script                                 
 # Features: Multi-channel support, flood protection, and real-time activity logging.      
 # Nickname / Context: asl_pls | irc.underx.org #aslpls                                     
 #
@@ -119,5 +119,5 @@ namespace eval ::ChanLimitPro {
         }
     }
     
-    putlog "Loaded ChanLimitPro successfully. Enable in channels using: .chanset #channel +dynamiclimit"
+    putlog "Loaded EggChanlimitPro.tcl successfully. Enable in channels using: .chanset #channel +dynamiclimit"
 }
